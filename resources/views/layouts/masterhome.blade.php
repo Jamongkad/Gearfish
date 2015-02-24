6f7510b82adfd103f3666d88de2cc3bee6124cdf
jamongkad <wrm932@gmail.com>
2015-02-24 21:20:52 +0800

    Adding title

--- resources/views/layouts/masterhome.blade.php
+++ resources/views/layouts/masterhome.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Gearfish: Convert CSV's into actionable data easily.</title>
         <!-- Optional theme -->
         <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
         <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
